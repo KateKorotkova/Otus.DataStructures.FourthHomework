8244e438d956c8a54f42d604b1a9dfd8c5ec5a2e
Ekaterina Korotkova <e.korotkova.2013@yandex.ru>
2022-05-20 20:29:30 +0300

    initial commit

---
+++ Otus.DataStructures.FourthHomework/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Otus.DataStructures.FourthHomework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
